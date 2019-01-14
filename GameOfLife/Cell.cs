fe68bfc12fa8b6b4a341564ca77b5467aebf7748
Keddy Shen <keddy.shen@myob.com>
2019-01-14 14:42:08 +1300

    Add States enum and State prop to Cell

--- GameOfLife/Cell.cs
+++ GameOfLife/Cell.cs
@@ -2,14 +2,20 @@ namespace GameOfLife
 {
     public class Cell
     {
-
-        public int X { get; }
-        public int Y { get; }
-
         public Cell(int x, int y)
         {
             X = x;
             Y = y;
         }
+
+        public int X { get; }
+        public int Y { get; }
+        public States State { get; set; }
+    }
+
+    public enum States
+    {
+        Live,
+        Dead
     }
 }
--- GameOfLifeTests/CellTests.cs
+++ GameOfLifeTests/CellTests.cs
@@ -6,7 +6,7 @@ namespace GameOfLifeTests
     public class CellShould
     {
         [Fact]
-        public void ReturnCellXLocation()
+        public void HaveXLocationProperty()
         {
             var cell = new Cell(1, 2);
             const int expected = 1;
@@ -15,14 +15,22 @@ namespace GameOfLifeTests
         }
         
         [Fact]
-        public void ReturnCellYLocation()
+        public void HaveYLocationProperty()
         {
             var cell = new Cell(1, 2);
             const int expected = 2;
             var actual = cell.Y;
             Assert.Equal(expected, actual);
         }
-        
-        
+
+        [Fact]
+        public void HaveStateProperty()
+        {
+            var cell = new Cell(1, 2) {State = States.Live};
+            const States expected = States.Live;
+            var actual = cell.State;
+            Assert.Equal(expected, actual);
+        }
+
     }
 }
